e67af1c67bdbeb4c0c96c1a0692a18c6ef51a974
Israel <israeloliveira171@gmail.com>
2023-10-10 14:04:23 -0300

    Implementando rotas no index

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const sequelize = require('./database/server');
-const Ocorrencia = require('./models/Ocorrencia');
+const ocorrenciaRoutes = require('./routes/ocorrenciaRoutes');
 
 const app = express();
 
+// Configuração do middleware para interpretar JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Endpoint para criar uma nova ocorrência
-app.post('/ocorrencias', async (req, res) => {
-    try {
-        const ocorr = await Ocorrencia.create(req.body);
-        res.status(201).send(ocorr);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+// Integração das rotas relacionadas às ocorrências
+app.use(ocorrenciaRoutes);
 
-// Endpoint para buscar todas as ocorrências
-app.get('/ocorrencias', async (req, res) => {
-    try {
-        const ocorrencias = await Ocorrencia.findAll();
-        res.status(200).send(ocorrencias);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-// Iniciar o servidor
+// Inicialização do servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
