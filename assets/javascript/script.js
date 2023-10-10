0e5d804eeee9bd84ace6b48c4332525c709adf3b
Israel <israeloliveira171@gmail.com>
2023-10-10 18:05:34 -0300

    Formulário e melhorias no script

---
+++ assets/javascript/script.js
@@ -0,0 +1,67 @@
+let marker;
+let map;
+let center = {lat: -6.890648554218208, lng: -38.554989416685295}; // Cajazeiras
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: center, 
+        zoom: 14,
+    });
+
+    marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        draggable: true
+    });
+
+    google.maps.event.addListener(marker, 'dragend', function(event) {
+        document.getElementById('latitude').value = event.latLng.lat();
+        document.getElementById('longitude').value = event.latLng.lng();
+    });
+}
+
+function placeMarker(location) {
+    marker.setPosition(location);
+    document.getElementById('latitude').value = location.lat();
+    document.getElementById('longitude').value = location.lng();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('formOcorrencia').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const data = {
+        titulo: document.getElementById('titulo').value,
+        tipo: document.getElementById('tipo').value,
+        data: document.getElementById('data').value,
+        hora: document.getElementById('hora').value,
+        localizacao: {
+            type: "Point",
+            coordinates: [
+                parseFloat(document.getElementById('longitude').value),
+                parseFloat(document.getElementById('latitude').value)
+            ]
+        }
+    };
+
+    fetch('/ocorrencias', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert('Ocorrência cadastrada com sucesso!');
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Erro ao cadastrar ocorrência.');
+    });
+});
+
+
+
+
+});
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
-const ocorrenciaRoutes = require('./routes/ocorrenciaRoutes');
+const cors = require('cors');
 
 const app = express();
-
 // Configuração do middleware para interpretar JSON
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(express.static(__dirname));
 
 // Integração das rotas relacionadas às ocorrências
+const ocorrenciaRoutes = require('./routes/ocorrenciaRoutes');
 app.use(ocorrenciaRoutes);
 
 // Inicialização do servidor
